270315ffe6c1f3836a65962dfc5c1a64dcc52696
Ishafizan Ishak <ishafizan@gmail.com>
2019-01-08 09:15:31 +0800

    Create tasks.py

---
+++ django/api/workers/tasks.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import logging
+import os.path
+import sys
+from json import dumps, loads
+from time import time
+import requests
+from celery.utils.log import get_task_logger
+
+logger = get_task_logger(__name__)
+
+try:
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    sys.path.append(BASE_DIR)
+    from workers import settings
+    from workers import util_es
+except ImportError as err:
+    logger.error(err)
+    raise Exception("import util files failed")
+
+
+# ----------------------------------------------
+# generic command to elasticsearch
+# ----------------------------------------------
+@settings.app.task(name="es_insert_generic", default_retry_delay=60, max_retries=3, acks_late=True, serializer='json')
+def es_insert_generic(data_out, es_target):
+    try:
+        # initialise elasticsearch client
+        es_client = util_es.init_es(logger, es_target["es_target"], es_target["es_target"],
+                                    es_target["es_target_index_audit"],
+                                    es_target["es_target_index_audit"])
+
+        util_es.insert_es_generic(logger, data_out, es_client["es_target_client"],
+                                  es_target["es_target_index_audit"])
+
+    except Exception as error:
+        logger.error(error)
+        raise ValueError("es_insert_generic failed")
