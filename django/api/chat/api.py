75ce4f7c741d8630d8c981d9e562b83664d97c8d
Ishafizan Ishak <ishafizan@gmail.com>
2019-01-09 14:05:46 +0800

    Update api.py

--- django/api/chat/api.py
+++ django/api/chat/api.py
@@ -9,6 +9,8 @@ from django.core.serializers.json import DjangoJSONEncoder
 try:
     sys.path.append(settings.BASE_DIR)
     from utils import util_log
+    from workers import tasks
+
     # instantiate logger
     log = util_log.logger()
 except ImportError:
@@ -20,16 +22,40 @@ except ImportError:
 # ----------------------------------
 @csrf_exempt
 def create_chat(request):
+    status_code = 400
     body_unicode = loads(request.body.decode('utf-8'))
-    log.info(dumps(body_unicode))
-    mymessage = body_unicode["mymessage"]
-    userid = body_unicode["userid"]
+    # log.info(dumps(body_unicode))
+    recipient_id = body_unicode["recipientId"]
+    sender_id = body_unicode["senderId"]
+    msg = body_unicode["msgContent"]
 
-    outdata = {"status_code": 200}
+    log.info("recipientId: %s" % recipient_id)
+    log.info("senderId: %s" % sender_id)
+    log.info("msgContent: %s" % msg)
 
-    # insert into database
+    # prepare data for insertion into es
+    data_out = {"recipient_id": recipient_id, "ip": sender_id,
+                "msg": msg}
 
-    response = get_response(request, outdata)
+    try:
+        """
+        queue_name = "chat"
+        # specify server & index_name
+        es_target = {"es_target": "localhost", "index": "my_index"}
+        # set you data structures to be inserted
+        log.info("Sending task to queue_name: %s " % queue_name)
+        tasks.es_insert_generic.apply_async(args=[data_out, es_target],
+                                            expires=1 * 60,
+                                            queue=queue_name)
+        """
+        status_code = 200
+        resp_msg = "chat inserted"
+    except Exception as err:
+        log.error(err)
+        resp_msg = err
+
+    resp_data = {"status_code": status_code, "msg": resp_msg}
+    response = get_response(request, resp_data)
     return response
 
 
