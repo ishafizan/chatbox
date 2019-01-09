7118211be66c89705ae639a97ca8b3a1061b2587
Ishafizan Ishak <ishafizan@gmail.com>
2019-01-09 14:07:08 +0800

    Update tests.py

--- django/api/chat/tests.py
+++ django/api/chat/tests.py
@@ -19,22 +19,54 @@ except ImportError:
 
 server = "localhost:8000"
 
-
 # Create your tests here.
 
-# imagine from react
-# python tests.py
-def test_body_json(mytoken):
+# -------------------------------------------
+# secure login
+# -------------------------------------------
+email = "get_from_me"
+password = 'get_from_me#@!'
+
+
+# login
+def login_secure():
+    base_url = "http://%s" % "get_from_me"
+    endpoint = "auth/login/secure"
+    final_url = "{0}/api/{1}/{2}/".format(base_url, "0.2", endpoint)
+    payload = {'email': email, 'password': password}
+    response = requests.post(final_url, data=payload)
+    data_token = response.json()
+    log.info(response.json())
+    if data_token['status'] == 200:
+        log.info("login success")
+        mytoken = data_token['data']['token']['access']
+        myuid = data_token['data']['userid']
+        return mytoken, myuid
+
+
+# create chat
+def test_create_chat(mytoken, mymsg):
     base_url = "http://%s" % server
     endpoint = "chat/create"
     final_url = "{0}/api/{1}/{2}/".format(base_url, "0.2", endpoint)
     log.info(final_url)
-    headers = {'Content-type': 'application/json'}
-    headers = {'Authorization': 'Bearer: %s' % mytoken}
-    payload = {"mymessage": "text in chat", "userid": 1234}
+    headers = {'Content-type': 'application/json', 'Authorization': 'Bearer: %s' % mytoken}
+    payload = {
+        'recipientId': 1234,
+        'senderId': 1,
+        'msgContent': msg
+    }
     response = requests.post(final_url, json=payload, headers=headers)
     return response
 
-mytoken = "123456789"
-res = test_body_json(mytoken)
+
+# login to get token
+log.info("Test: get token ...")
+token, userid = login_secure()
+log.info("token: %s" % token)
+log.info("userid: %s" % userid)
+
+# test creation of chat message
+msg = 'Hello world test message'
+res = test_create_chat(token, msg)
 log.info(res.json())
